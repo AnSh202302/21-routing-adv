796ca4da8b2a2815c7946d23b560d76490188c86
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-06-02 18:07:41 +0200

    add dummy events

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,4 +1,21 @@
+import EventsList from "../components/EventsList";
+
+const DUMMY_EVENTS = [
+  {
+    id: "e1",
+    title: "Event 1",
+  },
+  {
+    id: "e2",
+    title: "Event 2",
+  },
+  {
+    id: "e3",
+    title: "Event 3",
+  },
+];
+
 function EventsPage() {
-  return <h1>EventsPage</h1>;
+  return <EventsList events={DUMMY_EVENTS} />;
 }
 export default EventsPage;
